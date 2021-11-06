e334f650c9cb5be7e5249550bb0b2e2c8233f7fe
Seb1776 <sebastianwho1776@gmail.com>
2021-11-06 08:13:35 +0000

    console

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,31 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        args = shlex.split(args)
+        class_name = args[0] if args else None
+        args = args[1:]
+
+        if not class_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+
+        kwargs = {}
+        if args:
+            for arg in args:
+                arg = arg.split('=')
+                key, val = arg[0], arg[1]
+                if "_" in val:
+                    val = val.replace('_', ' ')
+                kwargs[key] = val
+            new_instance = HBNBCommand.classes[class_name](**kwargs)
+        else:
+            new_instance = HBNBCommand.classes[class_name]()
+        new_instance.save()
+
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -319,6 +334,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
